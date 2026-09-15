d9532636d374a3a89d365e46632a798baaa76283
mo3iiibest77-hub <mo3iiibest77@gmail.com>
2026-09-15 05:12:57 +0330

    chore: core module Gradle

---
+++ core/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.voxora.core"
+    compileSdk = 35
+    defaultConfig { minSdk = 29 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+}
