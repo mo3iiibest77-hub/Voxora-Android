0c5006edd42e65fc7cecc485a66a71a3d7e651f7
mo3iiibest77-hub <mo3iiibest77@gmail.com>
2026-09-15 05:21:11 +0330

    chore(phase1): OkHttp dependency for Gemini WebSocket

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -17,4 +17,5 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
